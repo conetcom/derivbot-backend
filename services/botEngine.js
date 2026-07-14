08068ad5b8ca091f218d0276e94c76d2aad97565
Wilmer Martinez <wilmerm@conetweb.com>
2026-07-14 07:49:44 -0500

    SE agrega tabla de estadisticas para analisis de estrategias

--- services/botEngine.js
+++ services/botEngine.js
@@ -388,7 +388,7 @@ expiry_time: new Date(Date.now() + 60000),
   status: "open"
   
 });
-console.log("estadisticas", trade.id);
+
 await saveTradeStatistics({
 
     tradeId: trade.id,
@@ -603,19 +603,10 @@ emitBalance(
     profit,
     exit_price:c.currentSpot
 });
-await updateTradeStatistics(
-
-    trade.id,
-
-    {
-
-        balanceAfter: riskManager.balance,
-
-        result: tradeResult
-
-    }
-
-);
+await updateTradeStatistics(trade.id, {
+    balanceAfter: risk.balance,
+    result
+});
 
       console.log(
         "✅ TRADE CERRADO EN DB:",
